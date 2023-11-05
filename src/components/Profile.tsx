03f2d08be4a2e248c58f99d089106ea07b966c56
sa556693828 <sa556693828@gmail.com>
2023-11-06 04:18:31 +0800

    Profile

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -9,13 +9,56 @@ import {View} from '@gluestack-ui/themed';
 const Profile = () => {
   const StyledBox = styled(Box);
   const StyledText = styled(Text);
+  const StyledVStack = styled(VStack);
+  const userInfo = {
+    first_name: 'N',
+    account: 'N',
+    email: 'N',
+    wallet_address: 'N',
+  };
+  const textStyle = 'uppercase text-sm tracking-[1.12px] font-normal';
 
   return (
-    <StyledBox className="flex flex-row flex-wrap gap-2">
-      <StyledBox className="w-44 h-44 bg-white/20 rounded-lg" />
-      <StyledBox className="w-44 h-44 bg-white/20 rounded-lg" />
-      <StyledBox className="w-44 h-44 bg-transparent border border-white/20 rounded-lg" />
-    </StyledBox>
+    <StyledVStack w="$full" gap="$4" tw="flex-1">
+      <VStack w="$full">
+        <StyledText className={`${textStyle} text-white/40`}>
+          Full Name
+        </StyledText>
+        <StyledBox className=" border-t border-white/20 py-4">
+          <StyledText className={`${textStyle} text-white`}>
+            {userInfo.first_name}
+          </StyledText>
+        </StyledBox>
+      </VStack>
+      <VStack w="$full">
+        <StyledText className={`${textStyle} text-white/40`}>
+          username
+        </StyledText>
+        <StyledBox className=" border-t border-white/20 py-4">
+          <StyledText className={`${textStyle} text-white`}>
+            {userInfo.account}
+          </StyledText>
+        </StyledBox>
+      </VStack>
+      <VStack w="$full">
+        <StyledText className={`${textStyle} text-white/40`}>email</StyledText>
+        <StyledBox className=" border-t border-white/20 py-4">
+          <StyledText className={`${textStyle} text-white`}>
+            {userInfo.email}
+          </StyledText>
+        </StyledBox>
+      </VStack>
+      <VStack w="$full">
+        <StyledText className={`${textStyle} text-white/40`}>
+          wallet address
+        </StyledText>
+        <StyledBox className=" border-t border-white/20 py-4">
+          <StyledText className={`${textStyle} text-white`}>
+            {userInfo.wallet_address}
+          </StyledText>
+        </StyledBox>
+      </VStack>
+    </StyledVStack>
   );
 };
 const styles = StyleSheet.create({
--- src/screens/ProfileScreen.tsx
+++ src/screens/ProfileScreen.tsx
@@ -1,13 +1,14 @@
 import {styled} from 'nativewind';
 import React from 'react';
 import Profile from '../components/Profile';
-import {Box, Text} from '@gluestack-ui/themed';
+import {Box, Button, Text} from '@gluestack-ui/themed';
 import LinearGradient from 'react-native-linear-gradient';
 
 const ProfileScreen = ({navigation}: any) => {
   const StyledBg = styled(LinearGradient);
   const StyledBox = styled(Box);
   const StyledText = styled(Text);
+  const StyledButton = styled(Button);
   const greyBg = '#6A7460';
   const yellowBg = '#B89950';
   return (
@@ -17,11 +18,16 @@ const ProfileScreen = ({navigation}: any) => {
       locations={[0, 0.45]}
       colors={[greyBg, '#131111']}
       className="flex h-full justify-center items-center">
-      <StyledBox className="flex h-full justify-start items-center text-left pt-[120px] px-4 w-full">
+      <StyledBox className="flex h-full justify-between items-center text-left pt-[120px] px-4 w-full pb-6">
         <StyledText className="text-white text-5xl font-semibold pb-20 w-full">
           Profile
         </StyledText>
         <Profile />
+        <StyledButton
+          className="w-full h-[61px] bg-white"
+          onPress={() => navigation.navigate('Login')}>
+          <StyledText className="text-black">Logout</StyledText>
+        </StyledButton>
       </StyledBox>
     </StyledBg>
   );
