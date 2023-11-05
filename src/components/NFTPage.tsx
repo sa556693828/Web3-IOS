39175610e5c129cf952329a343bc0955755eb62b
sa556693828 <sa556693828@gmail.com>
2023-11-06 04:39:08 +0800

    NFT page

--- src/components/NFTPage.tsx
+++ src/components/NFTPage.tsx
@@ -1,6 +1,13 @@
 import React, {useState, useEffect, useRef} from 'react';
 import {StyleSheet} from 'react-native';
-import {VStack, Spinner, Button, Text, Box} from '@gluestack-ui/themed';
+import {
+  VStack,
+  Spinner,
+  Button,
+  Text,
+  Box,
+  ScrollView,
+} from '@gluestack-ui/themed';
 import 'react-native-get-random-values';
 import '@ethersproject/shims';
 import {ethers} from 'ethers';
@@ -9,6 +16,7 @@ import axios from 'axios';
 import {styled} from 'nativewind';
 import LinearGradient from 'react-native-linear-gradient';
 import HistoryModal from './HistoryModal';
+import {Heart, ArchiveRestore} from 'lucide-react-native';
 
 const NFTPage = () => {
   const StyledBox = styled(Box);
@@ -45,22 +53,40 @@ const NFTPage = () => {
   }, []);
   const NFTSection = () => {
     if (loading) {
-      return <Spinner size="large" />;
+      return (
+        <StyledBox className="flex justify-center items-center w-full h-[358px] bg-yellow-10 border border-white/20 rounded-lg">
+          <Spinner />
+        </StyledBox>
+      );
     }
     return (
-      <VStack justifyContent="center" alignItems="center">
+      <StyledBox className="flex justify-center items-center w-full h-[358px] bg-yellow-100 rounded-lg">
         <Text>{tokenURI}</Text>
-      </VStack>
+      </StyledBox>
     );
   };
   return (
-    <VStack justifyContent="center" alignItems="center">
-      {NFTSection()}
+    <StyledBox className="flex h-full text-left pt-[120px] px-4 w-full space-y-10">
+      <VStack gap="$6">
+        <StyledBox className="flex flex-row justify-end items-center gap-6">
+          <ArchiveRestore size={28} color="#fffff050" />
+          <Heart size={28} color="#fffff050" />
+        </StyledBox>
+        {NFTSection()}
+        <StyledBox className="flex flex-row justify-between items-center">
+          <StyledText className="text-white/40 tracking-[1.28px] leading-[120%] font-normal">
+            YOHAKU FOUNDER'S EDITION
+          </StyledText>
+          <StyledText className="text-white/40 tracking-[1.28px] leading-[120%] font-normal">
+            #2
+          </StyledText>
+        </StyledBox>
+      </VStack>
       <Button onPress={() => setShowModal(true)} ref={ref}>
         <Text>Show Modal</Text>
       </Button>
       <HistoryModal showModal={showModal} setShowModal={setShowModal} />
-    </VStack>
+    </StyledBox>
   );
 };
 const styles = StyleSheet.create({
